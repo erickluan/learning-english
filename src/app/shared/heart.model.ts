c98cad32aa65d628922fab7b190cc24f9385eeb6
Erick Luan <erickluang@gmail.com>
2018-06-03 00:42:15 -0300

    New correct path to urls of hearts

--- src/app/shared/heart.model.ts
+++ src/app/shared/heart.model.ts
@@ -1,8 +1,8 @@
 export class Heart {
     constructor(
         public full: boolean,
-        public urlFullHeart = '/assets/full_heart.svg',
-        public urlEmptyHeart = '/assets/empty_heart.svg'
+        public urlFullHeart = 'assets/full_heart.svg',
+        public urlEmptyHeart = 'assets/empty_heart.svg'
     ) {}
     public showHeart(): string {
         if (this.full === true ) {
